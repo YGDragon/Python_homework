a788e21a80267b6ec51fe750bd6328aed9d6280c
ygdragon <djsdv@tut.by>
2022-10-12 22:26:05 +0300

    Task_4 - > Task_2

--- Lesson_2/Task_4.py
+++ Lesson_2/Task_4.py
@@ -8,50 +8,67 @@
 # Произведение для [4, 1, 4, 1] равно 16
 # Примечание: Границы диапазона вводятся пользователем, надо корректно учесть, что они могут быть некорректными:
 # больше длины списка, меньше нуля, первый больше второго и т.п.
-# проверка значений границ диапазона
-
-# нахождение произведения элементов
-# в ограниченном диапазоне
-def n_mult(data):
-    n = r_check(data)
-    data_cut = data[n[0] : n[1] + 1]
-    from math import prod
-    print(f'Произведение для {data_cut} равно {prod(data_cut)}')
-    
 
 # формирование списка элементов
 def lst_inp(num):
     from random import randint
     lst = [randint(-num, num) for i in range(-num, num + 1)]
-    print(lst)
+    print(f'список элементов {lst}')
     return lst
 
+# нахождение произведения элементов списка
+def n_mult(data):
+    n = r_check(data)
+    data_cut = data[n[0]: n[1] + 1]
+    from math import prod
+    print(f'индексы от {n[0]} до {n[1]} -> произведение для {data_cut} равно {prod(data_cut)}')
 
+# проверка ввода индексов
 def r_check(data):
     lst = [0, 0]
-    lst[0] = inp_check(input('Нижняя граница: '))
-    lst[1] = inp_check(input('Верхняя граница: '))
-    while lst[0] >= lst[1]:
+    lst[0] = n1_check(inp_check(input('нижний индекс: ')))
+    while lst[0] > len(data) - 1:
         print('Ошибка ввода!')
-        lst[0] = inp_check(input('Нижняя граница: '))
-    while (lst[1] > len(data) or lst[1] == 0):
+        lst[0] = n1_check(inp_check(input(f'- > требуется ввод нижнего индекса < {len(data)}: ')))
+    lst[1] = n0_check(inp_check(input('верхний индекс: ')))
+    while (lst[1] > len(data) or lst[0] >= lst[1]):
         print('Ошибка ввода!')
-        lst[1] = inp_check(input('Верхняя граница: '))
+        if lst[1] > len(data):
+            print(f'- > требуется ввод верхнего индекса < {len(data)}', end='')
+        elif lst[0] >= lst[1]:
+            print(f'- > требуется ввод верхнего индекса > {lst[0]}', end='')
+        lst[1] = n0_check(inp_check(input()))
     return lst
-    
-# проверка числа при вводе
+
+# проверка ввода чисел
 def inp_check(value):
-    while (type(value) == str or int(value) < 0):
+    while type(value) == str:
         try:
             int(value)
         except ValueError:
             print('Ошибка ввода!', end=' ')
-            print(f'Требуется ввести целое положительное число : ', end='')
+            print('Требуется ввести целое число : ', end='')
             value = input()
         else:
             break
     return int(value)
 
+# проверка ввода чисел
+def n0_check(value):
+    while value <= 0:
+        print('Ошибка ввода!', end=' ')
+        print(f'Введите число > ноля: ', end='')
+        value = inp_check(input())
+    return int(value)
 
-num = inp_check(input('Введите число: '))
+# проверка ввода чисел
+def n1_check(value):
+    while value < 0:
+        print('Ошибка ввода!', end=' ')
+        print(f' Введите число >= 0: ', end='')
+        value = inp_check(input())
+    return int(value)
+
+
+num = n0_check(inp_check(input('Введите число: ')))
 n_mult(lst_inp(num))
