989d0e6dc25d072eb039db87fb77146aad772a56
Дмитрий <109813121+YGDragon@users.noreply.github.com>
2022-10-12 20:11:05 +0300

    Update Task_4

--- Lesson_2/Task_4.py
+++ Lesson_2/Task_4.py
@@ -8,3 +8,32 @@
 # Произведение для [4, 1, 4, 1] равно 16
 # Примечание: Границы диапазона вводятся пользователем, надо корректно учесть, что они могут быть некорректными:
 # больше длины списка, меньше нуля, первый больше второго и т.п.
+# проверка значений границ диапазона
+def r_check(data):
+    lst = [0, 0]
+    lst[0] = inp_check(input('Нижняя граница: '))
+    lst[1] = inp_check(input('Верхняя граница: '))
+    while lst[0] >= lst[1]:
+        print('Ошибка ввода!')
+        lst[0] = inp_check(input('Нижняя граница: '))
+    while (lst[1] > len(data) or lst[1] == 0):
+        print('Ошибка ввода!')
+        lst[1] = inp_check(input('Верхняя граница: '))
+    return lst
+    
+# проверка числа при вводе
+def inp_check(value):
+    while (type(value) == str or int(value) < 0):
+        try:
+            int(value)
+        except ValueError:
+            print('Ошибка ввода!', end=' ')
+            print(f'Требуется ввести целое положительное число : ', end='')
+            value = input()
+        else:
+            break
+    return int(value)
+
+
+num = inp_check(input('Введите число: '))
+n_mult(lst_inp(num))
