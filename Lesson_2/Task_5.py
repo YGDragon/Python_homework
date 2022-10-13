1e5b3bfdbfc4fc9db6bd4e08f3713b1c727d24e9
Дмитрий <109813121+YGDragon@users.noreply.github.com>
2022-10-13 22:24:04 +0300

    Create Task_5

--- Lesson_2/Task_5.py
+++ Lesson_2/Task_5.py
@@ -4,3 +4,41 @@
 от первого до последнего и меняет текущий
 элемент со случайным элементом из правой части
 местами.
+
+def sequence(lst):
+    from random import randint
+    for i in range(6):
+        lst.append(randint(0, 6))
+    return lst
+    
+def mix(lst):
+    from random import choice
+    for i in range(len(lst)):
+        n0 = lst[i]
+        cnt = lst.count(n0)
+        print(f'cnt -> {cnt}')
+        lst.pop(i)
+        print(f'i -> {i}', end='   ')
+        print(f'n0 -> {n0}')
+        print(f'промежуточный {lst}')
+        n1 = 0
+        idx = 0
+        if n0 not in lst:
+            cnt0 = 0
+            n1 = choice(lst)
+            idx = lst.index(n1)
+            print(f'idx -> {idx}', end='   ')
+            print(f'n1 -> {n1}')
+            lst.insert(i, n1)
+            lst.pop(idx + 1)
+            lst.insert(idx  + 1, n0)
+        else:
+            lst.insert(i, n0)
+        print(f'новый -> {lst}')
+        print('')
+    return lst
+    
+lst = []
+print(lst)
+print(sequence(lst))
+print(f'перемешанный список -> {mix(lst)}')
