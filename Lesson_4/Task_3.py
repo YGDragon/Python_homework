5c76fa50e80339766d8b0998467791ec4294f832
ygdragon <djsdv@tut.by>
2022-10-24 22:25:19 +0300

    Code optimization for Task_3 in Lesson_4

--- Lesson_4/Task_3.py
+++ Lesson_4/Task_3.py
@@ -6,8 +6,4 @@
 from random import randint
 seq = [randint(1, 20) for _ in range(1, 10)]
 print(f'исходный список  -> {seq}')
-new_seq = set(seq)
-seq.clear()
-for i in new_seq:
-    seq.append(i)
-print(f'найденный список -> {seq}')
+print(f'найденный список -> {list(set(seq))}')
